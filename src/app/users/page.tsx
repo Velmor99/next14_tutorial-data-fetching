2f15ead7eb5d4f0805b40ea378261ab90cf7c20a
Velmor99 <wincentwangog@gmail.com>
2024-07-04 23:33:55 +0300

    loading and error states

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,7 +7,8 @@ type User = {
 };
 
 export default async function UsersPage() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users123");
   const users = await response.json();
   console.log(users);
   return (
