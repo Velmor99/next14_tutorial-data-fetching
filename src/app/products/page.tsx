078698449b90902181b4e2a148311f731eb4069e
Velmor99 <wincentwangog@gmail.com>
2024-07-07 23:57:51 +0300

    opting out of caching

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,6 @@
+import { cookies } from "next/headers";
+// export const fetchCache = "default-cache";
+
 type Product = {
   id: number;
   title: string;
@@ -6,7 +9,19 @@ type Product = {
 };
 
 export default async function ProductsPage() {
-  const response = await fetch("http://localhost:3001/products");
+  const detailsResponse = await fetch("http://localhost:3001/products/1"); // has been cached and reused
+  const details = await detailsResponse.json();
+
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+
+  const response = await fetch("http://localhost:3001/products", {
+    cache: "no-store",
+  });
+
+  const detailsResponse2 = await fetch("http://localhost:3001/products/2"); // hasn't been cached and reused becouse position bellow "no-store"
+  const details2 = await detailsResponse2.json();
+
   const products = await response.json();
   return (
     <ul className="space-y-4 p-4">
@@ -18,6 +33,7 @@ export default async function ProductsPage() {
           <h2 className="text-xl font-semibold">{product.title}</h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
+          <p>{details.price}</p>
         </li>
       ))}
     </ul>
