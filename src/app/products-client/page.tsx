32af879e63399c76e37dfe22e3c51237850953b2
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2024-08-18 23:41:20 +0300

    Update page.tsx

--- src/app/products-client/page.tsx
+++ src/app/products-client/page.tsx
@@ -45,18 +45,20 @@ const ProductsPage: React.FC = () => {
   }
 
   return (
-    <ul className="space-y-4 p-4">
-      {products.map((product: Product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-xl font-semibold">{product.title}</h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="space-y-4 p-4">
+        {products.map((product: Product) => (
+          <li
+            key={product.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            <h2 className="text-xl font-semibold">{product.title}</h2>
+            <p>{product.description}</p>
+            <p className="text-lg font-medium">${product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
